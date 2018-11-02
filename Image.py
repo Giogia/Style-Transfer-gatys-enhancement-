973897107d39f0bfc44170d4eb79216ac7f69ffb
giogia <Giovannitommasi95@gmail.com>
2018-11-02 16:26:42 +0800

    created main functions for uploading, pre/post processing and visualizing images

---
+++ Image.py
@@ -0,0 +1,99 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+from PIL import Image
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.vgg19 import preprocess_input
+
+
+#TODO test preprocessing and postprocessing functions
+
+
+def load_img(path):
+
+    max_dim = 512
+
+    path = os.getcwd() + path
+    img = Image.open(path)
+
+    #resize image to max_dim
+    scale = max_dim/max(img.size)
+    scaled_width = round(img.size[0]*scale)
+    scaled_height = round(img.size[1]*scale)
+    img = img.resize((scaled_width, scaled_height))
+
+    img = image.img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+
+    return img
+
+
+
+def load_preprocess_img(path):
+
+    img = load_img(path)
+
+    # adequate image to the format the model requires
+    img = preprocess_input(img)
+
+    return img
+
+
+
+def postprocess_img(processed_img):
+
+    img = processed_img.copy()
+
+    # Remove the batch dimension
+    img = np.squeeze(img, axis=0)
+
+    # shape (1, h, w, d) to (h, w, d)
+    img[:, :, 0] += 103.939
+    img[:, :, 1] += 116.779
+    img[:, :, 2] += 123.68
+
+    # rgb to bgr
+    img = img[:, :, ::-1]
+
+    img = np.clip(img, 0, 255).astype('uint8')
+
+    return img
+
+
+
+def show_img(img, title=None):
+
+    # Remove the batch dimension
+    out = np.squeeze(img, axis=0)
+
+    # Normalize for display
+    out = out.astype('uint8')
+
+    if title is not None:
+        plt.title(title)
+
+    plt.imshow(out)
+
+
+
+def show_content_style(content_path, style_path):
+
+    plt.figure(figsize=(10,10))
+
+    content = load_img(content_path)
+    style = load_img(style_path)
+
+    plt.subplot(1, 2, 1)
+    show_img(content, 'Content')
+
+    plt.subplot(1, 2, 2)
+    show_img(style, 'Style')
+
+    plt.show()
+
+
+
+if __name__ == "__main__":
+
+    show_content_style('/Images/Picture1.jpg','/Images/Picture2.jpg')
