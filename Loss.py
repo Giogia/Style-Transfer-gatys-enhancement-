ee39993f7b5dd7fc87d301039c1907c8b082034c
Andrea Perego <andrea14.perego@mail.polimi.it>
2018-11-14 14:15:48 +0800

    Fixed .gitignore and Loss
    Signed-off-by: Andrea Perego <andreaperegoit@gmail.com>

--- Loss.py
+++ Loss.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 
+
 def g_matrix(tensor):
     channels = int(tensor.shape()[-1])  # possible to use to static rep?
     a = tf.reshape(tensor, [-1, channels])  # reshape as 1-Dim array dividing it per channel
@@ -22,43 +23,6 @@ def get_style_loss(style, g_target):
     return tf.reduce_mean(tf.square(g_style - g_target))  # / weight
 
 
-def compute_loss(noise_features, img_features, loss_w, layers_n):
-    """This function will compute the loss total loss.
-
-    Arguments:
-      model: The model that will give us access to the intermediate layers
-      loss_w: The weights of each contribution of each loss function.
-        (style weight, content weight, and total variation weight)
-      init_img: Our initial base image. This image is what we are updating with
-        our optimization process. We apply the gradients wrt the loss we are
-        calculating to this image.
-      g_style_f: Precomputed gram matrices corresponding to the
-        defined style layers of interest.
-      content_f: Precomputed outputs from defined content layers of
-        interest.
-
-    Returns:
-      returns the total loss, style loss, content loss, and total variational loss
-    """
-
-    # Feed our init image through our model. This will give us the content and
-    # style representations at our desired layers. Since we're using eager
-    # our model is callable just like any other function!
-
-    # Accumulate style losses from all layers
-    # Accumulate content losses from all layers
-    content_score = accumulate_loss(img_features[0], layers_n[0], noise_features[0], get_content_loss)
-
-    # Here, we equally weight each contribution of each loss layer
-    style_score = accumulate_loss(img_features[1], layers_n[1], noise_features[1], get_content_loss)
-
-    content_score *= loss_w[0]
-    style_score *= loss_w[1]
-    # Get total loss
-    loss = style_score + content_score
-    return loss, style_score, content_score
-
-
 def accumulate_loss(img_feature, layers_n, noise_feature, loss):
     score = 0
     weight_per_layer = 1.0 / float(layers_n)
@@ -67,8 +31,33 @@ def accumulate_loss(img_feature, layers_n, noise_feature, loss):
     return score
 
 
+def compute_loss(noise_features, img_features, loss_w, layers_n):
+    """This function will compute the loss total loss.
+
+    Arguments:
+      noise_features: The content and style of the white noise
+      loss_w: The weights of each contribution of each loss function.
+        (style weight, content weight, and total variation weight)
+      img_features: Content and style features
+      loss_w: Weights of the elements
+      layers_n: Number of content and style layers
+
+    Returns:
+      returns the total loss
+    """
+
+    # Accumulate content losses from all layers
+    content_score = accumulate_loss(img_features[0], layers_n[0], noise_features[0], get_content_loss)
+    # Accumulate style losses from all layers
+    style_score = accumulate_loss(img_features[1], layers_n[1], noise_features[1], get_content_loss)
+    # Here, we equally weight each contribution of each loss layer
+    content_score *= loss_w[0]
+    style_score *= loss_w[1]
+    return style_score + content_score
+
+
 def compute_gradient(init_img, noise_features, img_features, loss_w, layers_n):
     with tf.GradientTape() as g:
-        all_loss = compute_loss(noise_features, img_features, loss_w, layers_n)
+        loss = compute_loss(noise_features, img_features, loss_w, layers_n)
         # Compute gradients wrt input image
-    return g.gradient(all_loss[0], init_img), all_loss
+    return g.gradient(loss, init_img), loss
