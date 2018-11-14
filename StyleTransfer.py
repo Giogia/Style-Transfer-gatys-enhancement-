acb38b88081b0ef18a18331da21e71b6f6236bdf
giogia <Giovannitommasi95@gmail.com>
2018-11-14 15:12:03 +0800

    style transfer function preprocess bug

--- StyleTransfer.py
+++ StyleTransfer.py
@@ -8,8 +8,12 @@ def run_style_transfer(content_path, style_path, iterations=1000, content_weight
     #create images
     content = Image.load_image(content_path)
     style = Image.load_image(style_path)
-
     noise = Image.generate_noise_image(content)
+
+    content = Image.preprocess_image(content)
+    style = Image.preprocess_image(style)
+    noise = Image.preprocess_image(noise)
+
     noise = tfe.Variable(noise, dtype=tf.float32)
 
     # create model
